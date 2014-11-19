944d3a51de7a93ec09b0d212f063f41c69d03c6b
Sidhant Uthra <sidhantuthra@dhcp-v880-0-01529.1Xwireless.unc.edu>
2014-11-19 17:20:42 -0500

    Initial Commit

---
+++ Apple Watch Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Apple Watch Test
+//
+//  Created by Sidhant Uthra on 11/19/14.
+//  Copyright (c) 2014 Sidhant Uthra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Apple Watch Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Apple Watch Test
+//
+//  Created by Sidhant Uthra on 11/19/14.
+//  Copyright (c) 2014 Sidhant Uthra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
